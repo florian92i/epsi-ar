27e33082dc5a8d78a56c24b58bd83f556812c353
Florian COMPIEGNE <florian.compiegne@eemi.com>
2021-02-08 16:31:25 +0100

    on est la

--- app/src/main/java/com/example/ar_epsi/MainActivity.java
+++ app/src/main/java/com/example/ar_epsi/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.ar_epsi;
 
-import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,10 +12,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
@@ -35,17 +34,21 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Random;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     ArFragment arFragment;
     boolean shouldAddModel = true;
+
+    private void register(Runnable r) {
+        findViewById(R.id.button).setOnClickListener(v -> r.run());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
         assert arFragment != null;
+        register(()-> {
+            try {
+                // image naming and path  to include sd card  appending name you choose for file
+                String mPath = Environment.getExternalStorageDirectory().toString() + "/" + "toto" + ".jpg";
+
+                // create bitmap screen capture
+                View v1 = getWindow().getDecorView().getRootView();
+                v1.setDrawingCacheEnabled(true);
+                Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+                v1.setDrawingCacheEnabled(false);
+
+                File imageFile = new File(mPath);
+
+                FileOutputStream outputStream = new FileOutputStream(imageFile);
+                int quality = 20;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+                outputStream.flush();
+                outputStream.close();
+
+            } catch (Throwable e) {
+                // Several error may come out with file handling or DOM
+                System.out.println("----------- anchor.getPose(); ----------------");
+                System.out.println(e);
+                e.printStackTrace();
+            }
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("image/jpg");
+            String shareSub = "Partager votre photo";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, "body text");
+            sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(Environment.getExternalStorageDirectory().toString() + "/" + "toto" + ".jpg"));
+            startActivity(Intent.createChooser(sharingIntent, "Share using"));
+        });
         arFragment.getPlaneDiscoveryController().hide();
         arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
     }
@@ -64,8 +100,11 @@ public class MainActivity extends AppCompatActivity {
                 .setSource(arFragment.getContext(), uri)
                 .build()
                 .thenAccept(modelRenderable -> {
+                    System.out.println("----------- anchor.getPose(); ----------------");
+                    System.out.println(anchor.getPose());
                     System.out.println("----------- COLOR ----------------");
                     ModelRenderable blueSphereRenderable = modelRenderable.makeCopy();
+
                     blueSphereRenderable.getMaterial(0).setFloat3("zone1Color", 1.00f, 1.00f, 1.00f);
                     blueSphereRenderable.getMaterial(1).setFloat3("zone1Color", 0.00f, 0.00f, 0.00f);
                     blueSphereRenderable.getMaterial(2).setFloat3("zone1Color", 1.00f, 1.00f, 1.00f);
@@ -103,13 +142,13 @@ public class MainActivity extends AppCompatActivity {
                         //augmentedImage
                         System.out.println("PASSE LA");
                         Image imageAcquire = frame.acquireCameraImage(); // ici faut passer la bonne taille d'image qui correpond à notre photo
-                        //image.getPlanes()[0].getBuffer(), image.getPlanes()[1].getBuffer()
-                        //image.getPlanes()[2].getBuffer(), image.getWidth(), image.getHeight()
-
-
-                        System.out.println(imageAcquire.getWidth());
-                        System.out.println(imageAcquire.getHeight());
-
+                        System.out.println("X");
+                        System.out.println(augmentedImage.getExtentX());
+                        System.out.println("Z");
+                        System.out.println(augmentedImage.getExtentZ());
+                        System.out.println("Center ^pose");
+                        System.out.println(Arrays.toString(augmentedImage.getCenterPose().getXAxis()));
+                        System.out.println(Arrays.toString(augmentedImage.getCenterPose().getYAxis()));
                         byte[] nv21 = convertYUV420ToN21(imageAcquire);
                         byte[] jpeg = convertN21ToJpeg(nv21, 640, 480);
                         // width X = 640
@@ -117,15 +156,11 @@ public class MainActivity extends AppCompatActivity {
                         imageAcquire.close();
                         // ECRIRE DANS UN FICHIER
                         Bitmap bitmapImage = BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length, null);
+                        System.out.println("GetDominant Color");
+                        getColorImg(bitmapImage, 320, 240);
+                        getColorImg(bitmapImage, 200, 150);
+
                         createDirectoryAndSaveFile(bitmapImage, "toto.jpeg");
-                        int pixel = bitmapImage.getPixel(12,35);
-                        int redValue = Color.red(pixel);
-                        int greenValue = Color.green(pixel);
-                        int blueValue = Color.blue(pixel);
-                        System.out.println("------------COLOR---------------");
-                        System.out.println(redValue);
-                        System.out.println(greenValue);
-                        System.out.println(blueValue);
 
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -236,5 +271,16 @@ public class MainActivity extends AppCompatActivity {
         } catch (final Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void getColorImg(Bitmap bitmap, int x, int y) {
+        int pixel = bitmap.getPixel(x,y);
+        int redValue = Color.red(pixel);
+        int greenValue = Color.green(pixel);
+        int blueValue = Color.blue(pixel);
+        System.out.println("------------COLOR---------------");
+        System.out.println(redValue);
+        System.out.println(greenValue);
+        System.out.println(blueValue);
     }
 }
